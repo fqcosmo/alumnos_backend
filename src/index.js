783a6119ed05b32faf61c8b27b1c8125f68fae37
fqcosmo <fabrizioquispe1900@gmail.com>
2024-07-03 14:20:37 -0500

    canbuis_nuevos_v10

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const {alumnos_router} = require('./routers/alumnos.router.js');
 
 const PORT = 3100;
 
 const App = express();
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: '200'
+}
 
 App.use(express.json());
 App.use('/api',alumnos_router);
